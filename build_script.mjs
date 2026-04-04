41b047fb0b7c98bcb83047434280d4a1d6bc1945
Gabriel Ramalho <gabrieldelai11ramalho@gmail.com>
2026-04-04 17:50:20 -0300

    moved all files to src/ so dist/ is exclusively for build output

--- build_script.mjs
+++ build_script.mjs
@@ -1,15 +1,31 @@
 import esbuild from "esbuild";
+import { htmlPlugin } from "@craftamap/esbuild-plugin-html";
+import * as fs from "fs/promises";
 
 const debug = ["true", "debug"].includes(process.argv[2]);
 
+console.log("cleaning build directory...");
+await fs.rm("dist", { recursive: true, force: true }).catch(() => {});
+
 console.log("building...");
 const ctx = await esbuild.context({
-    entryPoints: ["./src/index.js"],
+    entryPoints: ["./src/index.js", "./src/index.css"],
     minify: !debug,
     bundle: true,
-    outfile: "dist/index.js",
+    outdir: "dist",
     sourcemap: debug,
     format: "esm",
+    plugins: [
+        htmlPlugin({
+            files: [
+                {
+                    entryPoints: ["src/index.css", "src/index.ts"],
+                    filename: "index.html",
+                    htmlTemplate: "./src/index.html"
+                }
+            ]
+        })
+    ]
 });
 console.log("build finished!");
 
